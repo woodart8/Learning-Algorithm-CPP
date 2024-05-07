08309a92f31beb783ef2a83861808c28331c596a
Kibeom Jeon <kibeom1145@gmail.com>
2024-05-07 17:03:48 +0900

    백준

---
+++ 15665.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N,M;
+vector<int> v;
+vector<int> ans(7);
+
+void BT(int k) {
+    if(k == M) {
+        for(int i=0; i<M; i++) {
+            cout << ans[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0; i<v.size(); i++) {
+        ans[k] = v[i];
+        BT(k+1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> M;
+
+    int num;
+    for(int i=0; i<N; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(),v.end());
+    v.erase(unique(v.begin(),v.end()),v.end());
+    BT(0);
+
+    return 0;
+}
