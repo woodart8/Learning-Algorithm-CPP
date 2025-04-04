53df4c1ad791954e49bd7952e56944713be72c74
Kibeom Jeon <kibeom1145@gmail.com>
2025-04-04 15:29:56 +0900

    boj

--- 1111.cpp
+++ 1111.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -20,15 +19,23 @@ void solve() {
         cout << "A" << '\n';
         return;
     }
+    // N이 1일 경우 판정 불가
 
     if (N == 2) {
         if (v[0] == v[1]) cout << v[0] << '\n';
         else cout << "A" << '\n';
         return;
     }
+    // N이 2일 경우에 만약 두 숫자가 같으면 앞으로도 같은 숫자가 나와야 하지만
+    // 다를 경우에는 판정 불가
     
     int a = v[1]-v[0] != 0 ? (v[2]-v[1])/(v[1]-v[0]) : 0;
     int b = v[1] - v[0]*a;
+    // v[1]*a + b = v[2]
+    // v[0]*a + b = v[1]
+    // a = (v[2]-v[1]) / (v[1]-v[0]) (단 v[1] != v[0])
+    // b = v[1] - v[0]*a
+
     bool flag = true;
 
     for (int i=0; i<v.size()-1; i++) {
