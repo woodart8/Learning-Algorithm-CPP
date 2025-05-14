b7cc2f811df1f3f75518a0f60670f3e6841c6ae3
Kibeom Jeon <kibeom1145@gmail.com>
2025-05-14 12:52:17 +0900

    boj

---
+++ 1275.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, Q;
+vector<long long> tree;
+
+void update(int node, int start, int end, int idx, int val) {
+    if (idx > end || idx < start) return;
+    if (start == end) tree[node] = val;
+    else {
+        update(node*2, start, (start+end)/2, idx, val);
+        update(node*2+1, (start+end)/2+1, end, idx, val);
+        tree[node] = tree[node*2] + tree[node*2+1];
+    }
+}
+
+long long query(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return tree[node];
+    long long lval = query(node*2, start, (start+end)/2, left, right);
+    long long rval = query(node*2+1, (start+end)/2+1, end, left, right);
+    return lval + rval;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> Q;
+    tree.resize(4*N);
+    for (int i=1; i<=N; i++) {
+        int num;
+        cin >> num;
+        update(1, 1, N, i, num);
+    }
+
+    for (int i=1; i<=Q; i++) {
+        int x, y, a, b;
+        cin >> x >> y >> a >> b;
+        if (x > y) swap(x, y);
+        cout << query(1, 1, N, x, y) << '\n';
+        update(1, 1, N, a, b);
+    }
+
+    return 0;
+}
