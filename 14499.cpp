3f82d38b86f7c318ded509627ab5d48a8e009fab
Kibeom Jeon <kibeom1145@gmail.com>
2025-06-11 22:17:48 +0900

    boj

--- 14499.cpp
+++ 14499.cpp
@@ -1,76 +1,121 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int N,M,x,y,K;
+int N, M, x, y, K;
 int map[20][20];
-vector<int> num(7,0);
-queue<int> dir;
-int dx[] = {0, 0, 0, -1, 1}; //1: 동, 2: 서, 3: 북, 4: 남
-int dy[] = {0, 1, -1, 0, 0};
-int top = 1, bottom = 6, east = 3, west = 4, north = 2, south = 5;
-int count = 0;
-void simulation() {
-    while(!dir.empty()) {
-        int idx = dir.front();
-        dir.pop();
-        count++;
-        int next_x = x+dx[idx];
-        int next_y = y+dy[idx];
-        if(next_x < 0 || next_x > N-1 || next_y < 0 || next_y > M-1) continue;
-        x = next_x;
-        y = next_y;
-        switch (idx) {
-            case 1:
-                swap(num[top], num[west]);
-                swap(num[west], num[bottom]);
-                swap(num[bottom], num[east]);
-                break;
-            case 2:
-                swap(num[top], num[east]);
-                swap(num[east], num[bottom]);
-                swap(num[bottom], num[west]);
-                break;
-            case 3:
-                swap(num[top], num[south]);
-                swap(num[south], num[bottom]);
-                swap(num[bottom], num[north]);
-                break;
-            case 4:
-                swap(num[top], num[north]);
-                swap(num[north], num[bottom]);
-                swap(num[bottom], num[south]);
-                break;
+
+class Dice {
+    public:
+        int x, y; // 주사위 위치
+        int top, bottom, front, back, left, right; // 상, 하, 전, 후, 좌, 우
+
+        // 생성자
+        Dice(int _x, int _y, int _top, int _bottom, int _front, int _back, int _left, int _right) {
+            this->x = _x;
+            this->y = _y;
+            this->top = _top;
+            this->bottom = _bottom;
+            this->front = _front;
+            this->back = _back;
+            this->left = _left;
+            this->right = _right;
         }
 
-        if(map[x][y] > 0) {
-            num[bottom] = map[x][y];
-            map[x][y] = 0;
+        // 굴리는데 성공할 경우 true, 실패하면 false
+        bool roll(int dir) {
+            if(!checkValid(dir)) return false; // 벽에 막혀 못 굴리는 경우
+
+            // 굴리는 방향 : 동쪽 1, 서쪽 2, 북쪽 3, 남쪽 4
+            int temp;
+            switch (dir) {
+                case 1: temp = right;
+                        right = top;
+                        top = left;
+                        left = bottom;
+                        bottom = temp;
+                        y++;
+                        return true;
+                case 2: temp = left;
+                        left = top;
+                        top = right;
+                        right = bottom;
+                        bottom = temp;
+                        y--;
+                        return true;
+                case 3: temp = back;
+                        back = top;
+                        top = front;
+                        front = bottom;
+                        bottom = temp;
+                        x--;
+                        return true;
+                case 4: temp = front;
+                        front = top;
+                        top = back;
+                        back = bottom;
+                        bottom = temp;
+                        x++;
+                        return true;
+            }
+
+            return false;
         }
-        else map[x][y] = num[bottom];
-        cout << num[top];
-        if(count < K) cout << '\n';
-    }
 
-    return;
-}
+        // 주사위를 굴렸을 때, 이동한 칸에 쓰여 있는 수가 0이면, 주사위의 바닥면에 쓰여 있는 수가 칸에 복사된다. 
+        // 0이 아닌 경우에는 칸에 쓰여 있는 수가 주사위의 바닥면으로 복사되며, 칸에 쓰여 있는 수는 0이 된다.
+        void update() {
+            if (map[x][y] == 0) map[x][y] = this->bottom;
+            else {
+                this->bottom = map[x][y];
+                map[x][y] = 0;
+            }
+        }
 
-int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
-    int d;
+    private:
+        // 다음 이동 위치가 벽에 막혀있으면 false, 그 외에는 true
+        bool checkValid(int dir) {
+            switch (dir) {
+                case 1: if (y == M-1) return false;
+                        break;
+                case 2: if (y == 0) return false;
+                        break;
+                case 3: if (x == 0) return false;
+                        break;
+                case 4: if (x == N-1) return false;
+                        break;
+            }
+            return true;
+        }
+};
+
+void input() {
     cin >> N >> M >> x >> y >> K;
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<M; j++) {
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
             cin >> map[i][j];
         }
     }
-    for(int i=0; i<K; i++) {
-        cin >> d;
-        dir.push(d);
-    }
+}
 
-    simulation();
+void solve() {
+    Dice dice(x,y,0,0,0,0,0,0);
+    for (int i=0; i<K; i++) {
+        int op;
+        cin >> op;
+        // 굴리는 게 성공할 경우
+        if (dice.roll(op)) {
+            dice.update(); // 주사위 업데이트
+            cout << dice.top << '\n';
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    input();
+    solve();
     return 0;
 }
