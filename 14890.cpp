eb881dfc6456b606c0d155f1d61036f66c7f35bc
Kibeom Jeon <kibeom1145@gmail.com>
2025-06-11 14:03:56 +0900

    boj

--- 14890.cpp
+++ 14890.cpp
@@ -1,109 +1,82 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<cstring>
+#include <iostream>
+
 using namespace std;
- 
-int N;
-int L;
+
+int N, L;
 int map[100][100];
-bool check[100];
-vector<int> v;
- 
-bool IsPossible()
-{
-    // 초기화
-    memset(check, false, sizeof(check));
- 
-    for (int i = 0; i < N - 1; i++)
-    {
-        int cur = v[i];
-        int next = v[i + 1];
-        int sub = v[i] - v[i + 1];
-        // 같을 때(평지)
-        if (sub == 0)
-            continue;
-        // 왼쪽 경사로, 내리막길
-        else if (sub == 1)
-        {
-            for (int j = i + 1; j <= i + L; j++)
-            {
-                if (j >= N || check[j] || v[j] != next)
-                    return false;
-                else
-                {
-                    check[j] = true;
-                }
+
+bool checkPath(int* arr) {
+    int flat = 1;
+    bool isSkipped = false;
+
+    for (int i=1; i<N; i++) {
+        int diff = arr[i] - arr[i - 1];
+        // 평지
+        if (diff == 0) {
+            flat++;
+            if (isSkipped && flat >= L) {
+                isSkipped = false;
+                flat = 0;
             }
-        }
-        // 오른쪽 경사로, 오르막길
-        else if (sub == -1)
-        {
-            for (int j = i; j > i - L; j--)
-            {
-                if (j < 0 || check[j] || v[j] != cur)
-                    return false;
-                else
-                {
-                    check[j] = true;
-                }
+        } 
+        // 오르막
+        else if (diff == 1) {
+            if (flat >= L) flat = 1;
+            else return false;
+        } 
+        // 내리막
+        else if (diff == -1) {
+            if (isSkipped) return false;
+
+            // 즉시 설치 가능한 경우
+            if (L == 1) {
+                flat = 0;
+                continue;
             }
+            
+            isSkipped = true;
+            flat = 1;
         }
-        // 경사로 만들어지지 못함
-        else
-            return false;
+        else return false;
     }
- 
-    return true;
+
+    return !isSkipped;
 }
- 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
- 
+
+void input() {
     cin >> N >> L;
- 
-    int result = 0;
- 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
             cin >> map[i][j];
         }
     }
+}
+
+void solve() {
+    int answer = 0;
+    int temp[100];
+
     // 행 검사
-    for (int i = 0; i < N; i++)
-    {
-        v.clear();
- 
-        for (int j = 0; j < N; j++)
-        {
-            v.push_back(map[i][j]);
-        }
- 
-        if (IsPossible())
-        {
-            result += 1;
-        }
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++)
+            temp[j] = map[i][j];
+        if (checkPath(temp)) answer++;
     }
+
     // 열 검사
-    for (int i = 0; i < N; i++)
-    {
-        v.clear();
- 
-        for (int j = 0; j < N; j++)
-        {
-            v.push_back(map[j][i]);
-        }
- 
-        if (IsPossible())
-        {
-            result += 1;
-        }
+    for (int j=0; j<N; j++) {
+        for (int i=0; i<N; i++)
+            temp[i] = map[i][j];
+        if (checkPath(temp)) answer++;
     }
- 
-    cout << result;
+
+    cout << answer << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    input();
+    solve();
     return 0;
 }
